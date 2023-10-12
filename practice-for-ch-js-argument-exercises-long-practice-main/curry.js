ae71ac4490617474d5e65e6d7ccdd38b8699312f
DispicableLee <lee.robert053@gmail.com>
2023-10-12 15:29:03 -0400

    done

--- practice-for-ch-js-argument-exercises-long-practice-main/curry.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/curry.js
@@ -8,8 +8,41 @@ function continuousAdd(num){
     }
 }
 
-const calcA = continuousAdd()
+// const calcA = continuousAdd()
 
-console.log(calcA(1))
-console.log(calcA(1))
-console.log(calcA(2))
+// console.log(calcA(1))
+// console.log(calcA(1))
+// console.log(calcA(2))
+
+
+function currySum(num){
+    let argLength = num
+    let numbers = []
+    return function curriedSum(num){
+        numbers.push(num)
+        if(numbers.length===argLength){
+            console.log(numbers.reduce((acc, el)=> acc+el))
+        }else{
+            return curriedSum
+        }
+    }
+}
+
+
+Function.prototype.myCurry = function(numArgs){
+    const func = this
+    let argLength = numArgs
+    let argArray = []
+    return function curry(arg){
+        argArray.push(arg)
+        if(argArray.length===argLength){
+            console.log(argArray)
+            func.apply(this, argArray)
+        }else{
+            return curry
+        }
+    }
+}   
+
+const sum = continuousAdd.myCurry(3)
+console.log(sum(1)(2)(3)) 
